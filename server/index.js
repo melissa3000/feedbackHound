25bc1f26563c7d378d9e4e0ca39d39711485933b
Melissa Boyer <melissaboyer3000@gmail.com>
2018-12-26 11:31:29 -0500

    user schema created in db

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./services/passport');
+require('./models/User');
 
 // connect to mongoDB using mongoose
 mongoose.connect(keys.mongoURI);
