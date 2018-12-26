dc31458acd093e49d4907266960f943b4acb24ae
Melissa Boyer <melissaboyer3000@gmail.com>
2018-12-26 11:53:19 -0500

    new user successfully added to db

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 // Create a new instance of the google passport strategy and point passport to it to enable google oAuth
 passport.use(
@@ -11,9 +14,8 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		}, 
 		(accessToken, refreshToken, profile, done) => {
-			console.log('access token', accessToken);
-			console.log('refresh token', refreshToken);
-			console.log('profile:', profile);
+			// create new user instance using the google profile id and save it to the db
+			new User({ googleId: profile.id }).save();
 		}
 	)
 );
