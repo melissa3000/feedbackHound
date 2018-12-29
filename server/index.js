4b714ecc9f5d8eafee526a041fad6ffb2d518312
Melissa Boyer <melissaboyer3000@gmail.com>
2018-12-29 11:36:48 -0500

    Add route to test cookie session is successful

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
-
 
 // connect to mongoDB using mongoose
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+	// cookie expires in 30 days and will be automatically encrypted
+	cookieSession({ 
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // require function from authRoutes and call it with app to allow routes to access app and run routes
 require('./routes/authRoutes')(app);
