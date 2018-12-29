e9deba5a39f84a5979cf02f27022d79fb9dc074c
Melissa Boyer <melissaboyer3000@gmail.com>
2018-12-29 11:08:15 -0500

    Serialize and deserialize user by id

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id)
+		.then(user => {
+			done(null, user);
+		});
+});
+
 // Create a new instance of the google passport strategy and point passport to it to enable google oAuth
 passport.use(
 	new GoogleStrategy(
@@ -17,12 +28,17 @@ passport.use(
 			// make sure user does not already exist by verifying google id does not match
 			// existing profile id
 			User.findOne({ googleId: profile.id })
-				.then((existingUser) => {
+				.then(existingUser => {
 					if (existingUser) {
 						// profile id already exists in db
+						// Call the done function to let passport know authentication is finished
+						// parameters for done are (errors, userRecord)
+						done(null, existingUser);
 					} else {
 						// make a new record and save it to the db
-						new User({ googleId: profile.id }).save();
+						new User({ googleId: profile.id })
+							.save()
+							.then(user => done(null, user));
 					}
 			});
 			
