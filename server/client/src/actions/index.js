f82a6108e352572ce3d00a90ee0a98bf0e30dd5d
Melissa Boyer <melissaboyer3000@gmail.com>
2019-01-03 20:58:43 -0500

    Wire up fetchUser action to verify user is logged in when using app

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -4,6 +4,9 @@ import { FETCH_USER } from './types';
 // action creator initiates change inside of the redux side of the application.
 // used to modify state contained in redux store
 
-const fetchUser = () => {
-	axios.get('/api/current_user');
+// wire up action creator fetchUser to application
+export const fetchUser = () => async dispatch => {
+		const res = await axios.get('/api/current_user');
+
+		dispatch({ type: FETCH_USER, payload: res })
 };
