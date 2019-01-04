75982f55627b42719fe043908433594b36d39465
Melissa Boyer <melissaboyer3000@gmail.com>
2019-01-04 10:08:12 -0500

    Added landing page component and finished header

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -4,9 +4,10 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+import Landing from './Landing';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+
 
 class App extends Component {
 	// makes a one time call the instant the application starts to load to see if user is logged in
